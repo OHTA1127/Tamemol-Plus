e544c1a9221ab6940209c612fbe665f58e793ee0
OHTA1127 <ohta.nba@gmail.com>
2023-10-12 21:02:10 +0900

    feat:summaryページのpagination作成

--- app/components/summary/table/summary-unbuy-table.tsx
+++ app/components/summary/table/summary-unbuy-table.tsx
@@ -11,6 +11,8 @@ import {
   Tr,
 } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
+import ReactPaginate from 'react-paginate'
+import './summary-table-paginate.css'
 
 type Product = {
   category: string | null
@@ -52,6 +54,27 @@ function SummaryUnbuyTable({ productData, selectMonth }: Props) {
     }
   }, [productData, selectMonth])
 
+  //1ページに何個表示するか
+  const productsPerPage = 7
+
+  //表示される最初の商品
+  const [productsOffset, setProductsOffset] = useState(0)
+
+  //次のページの最初に表示される商品
+  const endOffset = productsOffset + productsPerPage
+
+  const currentProducts = unBuyProductList?.slice(productsOffset, endOffset)
+  const pageCount = unBuyProductList
+    ? Math.ceil(unBuyProductList?.length / productsPerPage)
+    : 0
+
+  const handlePageClick = (e: { selected: number }) => {
+    const newOffset = unBuyProductList
+      ? (e.selected * productsPerPage) % unBuyProductList?.length
+      : 0
+    setProductsOffset(newOffset)
+  }
+
   return (
     <Box bg="white" borderRadius="10px" shadow="2xl" h="500px" w="550px" p={3}>
       <Text
@@ -73,7 +96,7 @@ function SummaryUnbuyTable({ productData, selectMonth }: Props) {
             </Tr>
           </Thead>
           <Tbody>
-            {unBuyProductList.map((item) => (
+            {currentProducts.map((item) => (
               <Tr key={item.id}>
                 <Td>{item.name}</Td>
                 <Td>{item.category}</Td>
@@ -83,6 +106,32 @@ function SummaryUnbuyTable({ productData, selectMonth }: Props) {
           </Tbody>
         </Table>
       </TableContainer>
+      {unBuyProductList.length >= 7 && (
+        <ReactPaginate
+          pageCount={pageCount}
+          onPageChange={handlePageClick}
+          marginPagesDisplayed={4} // 先頭と末尾に表示するページ数
+          pageRangeDisplayed={2} // 現在のページの前後をいくつ表示させるか
+          containerClassName="pagination justify-center" // ul(pagination本体)
+          pageClassName="page-item" // li
+          pageLinkClassName="page-link rounded-full" // a
+          activeClassName="active" // active.li
+          activeLinkClassName="active" // active.li < a
+          // 戻る・進む関連
+          previousClassName="page-item" // li
+          nextClassName="page-item" // li
+          previousLabel={'<'} // a
+          previousLinkClassName="previous-link"
+          nextLabel={'>'} // a
+          nextLinkClassName="next-link"
+          // 先頭 or 末尾に行ったときにそれ以上戻れ(進め)なくする
+          disabledClassName="disabled-button d-none"
+          // 中間ページの省略表記関連
+          breakLabel="..."
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
+        />
+      )}
     </Box>
   )
 }
