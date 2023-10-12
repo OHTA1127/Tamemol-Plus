1781a42a7fe933b79665b0be814a4e61a5ce2be8
OHTA1127 <ohta.nba@gmail.com>
2023-10-12 19:08:47 +0900

    fix:recordページのテーブルレイアウト変更

--- app/components/common/organisms/products/product-table.tsx
+++ app/components/common/organisms/products/product-table.tsx
@@ -135,13 +135,30 @@ function ProductTable({ currentMonthProducts }: Props) {
           <ReactPaginate
             pageCount={pageCount}
             onPageChange={handlePageClick}
-            previousLabel={'<'}
-            nextLabel={'>'}
-            breakLabel={'...'}
-            marginPagesDisplayed={2} // 一番最初と最後を基準にして、そこからいくつページ数を表示するか
-            pageRangeDisplayed={5} // アクティブなページを基準にして、そこからいくつページ数を表示するか
-            containerClassName={'pagination'} // ページネーションであるulに着くクラス名
-            activeClassName={'active'} // アクティブなページのliに着くクラス名
+            marginPagesDisplayed={4} // 先頭と末尾に表示するページ数
+            pageRangeDisplayed={2} // 現在のページの前後をいくつ表示させるか
+            containerClassName="pagination justify-center" // ul(pagination本体)
+            pageClassName="page-item" // li
+            pageLinkClassName="page-link rounded-full" // a
+            activeClassName="active" // active.li
+            activeLinkClassName="active" // active.li < a
+            
+            // 戻る・進む関連
+            previousClassName="page-item" // li
+            nextClassName="page-item" // li
+            previousLabel={'<'} // a
+            previousLinkClassName="previous-link"
+            nextLabel={'>'} // a
+            nextLinkClassName="next-link"
+           
+            // 先頭 or 末尾に行ったときにそれ以上戻れ(進め)なくする
+            disabledClassName="disabled-button d-none"
+           
+            // 中間ページの省略表記関連
+            breakLabel="..."
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
+        
           />
         </>
       ) : (
